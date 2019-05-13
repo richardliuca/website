c644b5f718e9937fb6cecd415e2f972365850665
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-05-13 15:19:44 -0700

    adding database model

---
+++ portfolio/models.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class Admin(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    profile_pic = db.Column(db.String(120), nullable=False, default='default-user.jpg')
+    projs = db.relationship('Project', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f'Admin({self.name}, {self.profile_pic}, {self.projs})'
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40), unique=True, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    documentation = db.Column(db.Text, nullable=False)
+    admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'), nullable=False)
