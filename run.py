385f38892a1f565e3bb83b04b43a8cee94b6e6ab
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-05-13 19:16:40 -0700

    adding several cli commands to create and reset admin

--- run.py
+++ run.py
@@ -1,4 +1,33 @@
+import portfolio, os, json
+app = portfolio.create_app()
+
+# with app.app_context():
+#     portfolio.db.create_all()
+
+@app.cli.command('create_db', with_appcontext=True)
+def create_db():
+    portfolio.db.create_all()
+
+@app.cli.command('reset_db', with_appcontext=True)
+def reset_database():
+    portfolio.db.drop_all()
+    portfolio.db.create_all()
+    create_admin()
+
+def create_admin():
+    from portfolio import Admin
+    from portfolio import bcrypt
+
+    file_path = os.path.join(app.instance_path, 'admin.json')
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        master = Admin(
+            name=data['name'],
+            email=data['email'],
+            password=bcrypt.generate_password_hash(data['password']))
+        portfolio.db.session.add(master)
+        portfolio.db.session.commit()
+
+
 if __name__ == '__main__':
-    import portfolio
-    app = portfolio.create_app()
     app.run()
