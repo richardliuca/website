61454b957dee81e77898bea1bb3c8ed88983cf8f
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-05-14 16:40:20 -0700

    adding home view and url registration

--- portfolio/front_page/__init__.py
+++ portfolio/front_page/__init__.py
@@ -1,6 +1,11 @@
 from flask import Blueprint
+from portfolio.front_page.views import Home
 
 front_page = Blueprint('front_page', __name__,
                         template_folder='templates',
                         static_folder='static',
                         static_url_path='/front_page/static')
+
+front_page.add_url_rule('/home/',
+            view_func=Home.as_view('home', template_name='home.html'))
+front_page.add_url_rule('/', endpoint='home')
--- portfolio/front_page/views.py
+++ portfolio/front_page/views.py
@@ -1,2 +1,22 @@
 from flask import render_template, url_for, flash, redirect, request, abort
 from jinja2 import TemplateNotFound
+
+
+from flask.views import View
+
+class GeneralView(View):
+    methods = ['GET',]
+    def __init__(self, template_name):
+        self._template_name = template_name
+
+    def render(self, *args, **kwargs):
+        try:
+            return render_template(self._template_name, *args, **kwargs)
+        except TemplateNotFound:
+            abort(404)
+
+    def dispatch_request(self, *args, **kwargs):
+        return self.render(*args, **kwargs)
+
+class Home(GeneralView):
+    pass
