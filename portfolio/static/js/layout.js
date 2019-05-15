433eac9b1409c5db8f7723ae0c8f3dd2ba37c486
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-05-15 00:30:30 -0700

    adding navbar and auto highlight current page

--- portfolio/static/js/layout.js
+++ portfolio/static/js/layout.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-  // $("#top-alert").fadeIn("fast").delay(3000).fadeOut("slow");
-  $(".alert").alert("close")
+  $(".alert").alert("close");
+  var title = $("title").html();
+  if (title == "Home" || title == "Projects Hub" || title == "About Me"){
+    $("a#nav-item").removeClass("active");
+    $("a#"+title.replace(" ", "-")).addClass("active");
+  } else {
+    $("a#nav-item").removeClass("active");
+  }
+
 
 })
