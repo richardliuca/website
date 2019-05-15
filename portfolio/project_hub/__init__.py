498cb6cf0230b88f367357e6cbf7974a10351f8f
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-05-14 19:50:10 -0700

    adding projects hub view and page

--- portfolio/project_hub/__init__.py
+++ portfolio/project_hub/__init__.py
@@ -1,6 +1,10 @@
 from flask import Blueprint
+from portfolio.project_hub.views import Hub
 
 project_hub = Blueprint('project_hub', __name__,
                         template_folder='templates',
                         static_folder='static',
                         static_url_path='/project_hub/static')
+
+project_hub.add_url_rule('/projects',
+                view_func=Hub.as_view('hub', 'projects.html'))
