77fdc4c006fec0b8cc882d6ae814a47b6a5613e1
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-05-15 01:17:41 -0700

    fixing alert auto fade

--- portfolio/static/js/layout.js
+++ portfolio/static/js/layout.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-  $(".alert").alert("close");
+  $(".alert-success").delay(5000).fadeTo(2000, 500).slideUp(500, function(){
+    $(".alert-success").slideUp(500);
+  });
+  $(".alert-info").delay(5000).fadeTo(2000, 500).slideUp(500, function(){
+    $(".alert-info").slideUp(500);
+  });
   var title = $("title").html();
   if (title == "Home" || title == "Projects Hub" || title == "About Me"){
     $("a#nav-item").removeClass("active");
