ba63dd7515ad2c504c2cd835fc9f85053fbf43ac
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-05-14 17:55:08 -0700

    changing home page url order

--- portfolio/front_page/__init__.py
+++ portfolio/front_page/__init__.py
@@ -6,6 +6,6 @@ front_page = Blueprint('front_page', __name__,
                         static_folder='static',
                         static_url_path='/front_page/static')
 
-front_page.add_url_rule('/home/',
+front_page.add_url_rule('/',
             view_func=Home.as_view('home', template_name='home.html'))
-front_page.add_url_rule('/', endpoint='home')
+front_page.add_url_rule('/home/', endpoint='home')
