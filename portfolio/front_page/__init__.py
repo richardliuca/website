cfd73ed10c5f4e3cc71e38500c43dd49caecaf5b
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-05-14 19:49:04 -0700

    adding about me view and page

--- portfolio/front_page/__init__.py
+++ portfolio/front_page/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from portfolio.front_page.views import Home
+from portfolio.front_page.views import Home, About
 
 front_page = Blueprint('front_page', __name__,
                         template_folder='templates',
@@ -9,3 +9,5 @@ front_page = Blueprint('front_page', __name__,
 front_page.add_url_rule('/',
             view_func=Home.as_view('home', template_name='home.html'))
 front_page.add_url_rule('/home/', endpoint='home')
+front_page.add_url_rule('/about/',
+            view_func=About.as_view('about', template_name='about.html'))
