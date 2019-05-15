719e27ea34ee31ee4a2ffa3d669991837918dbd8
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-05-15 00:13:18 -0700

    adding tite variable to pass in

--- portfolio/front_page/views.py
+++ portfolio/front_page/views.py
@@ -3,7 +3,8 @@ from jinja2 import TemplateNotFound
 from portfolio.views import GeneralView
 
 class Home(GeneralView):
-    pass
+    def dispatch_request(self):
+        return super().dispatch_request(title='Home')
 
 class About(GeneralView):
         def dispatch_request(self):
