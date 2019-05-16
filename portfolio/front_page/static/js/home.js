2656fdc4e802a9e7641fdc0192271a553fb705db
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-05-16 12:40:20 -0700

    passing cover image if found and replacing background with said cover

--- portfolio/front_page/static/js/home.js
+++ portfolio/front_page/static/js/home.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
   $("body").addClass("text-center");
+  $("body").css({
+    "background-image": `url(${$("#cover").html()})`,
+    "background-size": "cover",
+  });
+  console.log(typeof $("#cover").html());
+  console.log($("body").css("background-color"));
 })
--- portfolio/front_page/views.py
+++ portfolio/front_page/views.py
@@ -1,10 +1,25 @@
-from flask import url_for, flash, redirect, request, abort
+from flask import url_for, flash, redirect, request, abort, current_app
 from jinja2 import TemplateNotFound
 from portfolio.views import GeneralView
+import os, pathlib
+import os.path as path
 
 class Home(GeneralView):
     def dispatch_request(self):
-        return super().dispatch_request(title='Home')
+        cover_dir = path.join(current_app.instance_path, pathlib.Path('static/Home/'))
+        cover_path = url_for('front_page.static', filename='cover.jpeg', _external=True)
+        try:
+            for file in os.listdir(cover_dir):
+                if path.isfile(path.join(cover_dir, file)):
+                    name, ext = path.splitext(file)
+                    if name.lower() == 'cover':
+                        cover_path = url_for('file',
+                                            file='static/Home/'+file)
+                        break
+        except:
+            pass
+        print(cover_path)
+        return super().dispatch_request(title='Home', cover=cover_path)
 
 class About(GeneralView):
         def dispatch_request(self):
