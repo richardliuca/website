1930a74a170af0a7fd4b14e324f5875bd01f89c8
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-05-18 16:47:01 -0700

    passing sub blueprint name to card header

--- portfolio/notebook/views.py
+++ portfolio/notebook/views.py
@@ -5,12 +5,14 @@ class Notebook(GeneralView):
 
     def count_notes(self):
         try:
-            from portfolio.notes_catalog import notes_num
+            from portfolio.notes_catalog import notes_num, module_list
         except:
             notes_num = 0
 
-        return notes_num
+        return notes_num, module_list
 
     def dispatch_request(self):
-        notes_num = self.count_notes()
-        return super().dispatch_request(title='Notebook', notes_num=notes_num)
+        notes_num, modules = self.count_notes()
+        return super().dispatch_request(title='Notebook',
+                                        notes_num=notes_num,
+                                        modules=modules)
--- portfolio/project_hub/views.py
+++ portfolio/project_hub/views.py
@@ -8,13 +8,14 @@ class Hub(GeneralView):
 
     def count_proj(self):
         try:
-            from portfolio.project_catalog import proj_num
+            from portfolio.project_catalog import proj_num, module_list
         except:
             proj_num = 0
 
-        return proj_num
+        return proj_num, module_list
 
     def dispatch_request(self):
-        proj_num = self.count_proj()
-
-        return super().dispatch_request(title='Project Hub', proj_num=proj_num)
+        proj_num, modules = self.count_proj()
+        return super().dispatch_request(title='Project Hub',
+                                        proj_num=proj_num,
+                                        modules=modules)
