5549df77cb8b37a4e46520a3e072c2622d815a0b
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-05-18 16:22:16 -0700

    adding note database model and redefining relationship

--- portfolio/models.py
+++ portfolio/models.py
@@ -17,13 +17,21 @@ class Admin(db.Model, UserMixin):
     name = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(40), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    profile_pic = db.Column(db.String(120), nullable=False, default='default-user.jpg')
-    projs = db.relationship('Project', backref='author', lazy=True)
+    profile_pic = db.Column(db.String(120), nullable=True)
+    projs = db.relationship('Project', backref='lead', lazy=True)
+    notes = db.relationship('Note', backref='author', lazy=True)
 
     def __repr__(self):
-        return f'Admin({self.name}, {self.profile_pic}, {self.projs})'
+        return f'Admin({self.name}, {self.profile_pic}, {self.projs}, {self.notes})'
 
 class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40), unique=True, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    documentation = db.Column(db.Text, nullable=False)
+    admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'), nullable=False)
+
+class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(40), unique=True, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
