62a29b3e225b56806ed09bf257036895e10ae2fc
Richard Liu <37211328+richardliuca@users.noreply.github.com>
2019-05-18 23:53:40 -0700

    fixing typo

--- portfolio/notes_catalog/__init__.py
+++ portfolio/notes_catalog/__init__.py
@@ -2,7 +2,7 @@ from flask import current_app
 from pathlib import Path
 import os
 
-proj_num = 0
+notes_num = 0
 module_list = []
 
 proj_dir = Path(current_app.instance_path).joinpath('notes')
@@ -11,7 +11,7 @@ for folder in os.listdir(proj_dir):
     if folder.startswith('__'):
         pass
     elif proj_dir.joinpath(folder).is_dir():
-        proj_num += 1
+        notes_num += 1
         module_list.append({
             'name': 'Python',
             'description': 'I learnt python from Learn Python The Hard Way',
