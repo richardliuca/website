69249dbbc14a046ea770e25f76b5461cf42410d0
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-05-24 13:17:09 -0700

    handles additional query

--- portfolio/catalog/__init__.py
+++ portfolio/catalog/__init__.py
@@ -9,31 +9,17 @@ class Catalog(object):
     num = 0
     property_list = []
 
-    def __init__(self, source=None, page=1):
+    def __init__(self, source=None, page=1, **kwargs):
         if source is not None:
             proj_dir = Path(current_app.instance_path).joinpath(source)
 
         if source == 'projects':
-            self.posts = Project.query.order_by(Project.date_posted.desc()).paginate(page=page, per_page=9)
+            self.posts = Project.query.\
+            filter_by(complete=True, **kwargs).order_by(Project.date_posted.desc()).\
+            paginate(page=page, per_page=9)
         elif source == 'notes':
-            self.posts = Note.query.order_by(Note.date_posted.desc()).paginate(page=page, per_page=9)
+            self.posts = Note.query.\
+            filter_by(complete=True, **kwargs).order_by(Note.date_posted.desc()).\
+            paginate(page=page, per_page=9)
         else:
             raise Exception('Wrong post type')
-
-
-
-            # try:
-            #     for folder in os.listdir(proj_dir):
-            #         if folder.startswith('__'):
-            #             pass
-            #         elif proj_dir.joinpath(folder).is_dir():
-            #             self.num += 1
-            #             self.property_list.append({
-            #                 'name': 'Python',
-            #                 'description': 'I learnt python from Learn Python The Hard Way',
-            #                 'category': 'Programming Languange'
-            #             })
-            #         else:
-            #             pass
-            # except:
-            #     pass
