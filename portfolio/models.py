869541569e7ed605b0897292c6a8c6a6b5ccb72b
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-06-03 21:29:54 -0700

    modifying database to remove datetime relationship

--- portfolio/models.py
+++ portfolio/models.py
@@ -20,13 +20,12 @@ class Admin(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
-
 class Post(db.Model):
     __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True)
     complete = db.Column(db.Boolean, nullable=False)
-    tag = db.relationship('Tag', backref='post', lazy=True)
-    date_posted = db.relationship('Date', backref='post', lazy=True)
+    tags = db.relationship('Tag', backref='post', lazy=True)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     title = db.Column(db.String(60), unique=True, nullable=False)
     body = db.Column(db.Text, nullable=False)
     admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'), nullable=False)
@@ -35,9 +34,4 @@ class Tag(db.Model):
     __tablename__ = 'tag'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
-
-class Date(db.Model):
-    __tablename__ = 'date'
-    id = db.Column(db.DateTime, nullable=False, nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
