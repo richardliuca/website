f90afe635af09844205d73d747e17e4046a51699
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-06-17 14:21:36 -0700

    Changing the way dummy post are generated

--- run.py
+++ run.py
@@ -29,7 +29,6 @@ def create_admin():
 
             project_tag = Tag(name='project')
             note_tag = Tag(name='note')
-            # portfolio.db.session.add(master)
             portfolio.db.session.add_all([master, project_tag, note_tag])
             portfolio.db.session.commit()
 
@@ -45,20 +44,30 @@ def create_dummy_post():
     import random
     tag1 = Tag.query.get(1)
     tag2 = Tag.query.get(2)
-    tag3 = Tag(name='Hello')
-    tag4 = Tag(name='GoodBye')
-    tag5 = Tag(name='Ciao')
+    tag3 = Tag(name='hello')
+    tag4 = Tag(name='goodbye')
+    tag5 = Tag(name='ciao')
     for i in range(20):
         fir = [random.choice((tag1, tag2))]
         sec = [random.choice((tag3, tag4, tag5))]
         fir.extend(sec)
         new_post = Post(complete=True,
                         tags=fir,
+                        date_posted=datetime.now(),
                         title= f'Test Title {i}',
-                        body=datetime.utcnow(),
+                        body='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                         admin_id=1)
         portfolio.db.session.add(new_post)
         portfolio.db.session.commit()
+
+@app.cli.command('search', with_appcontext=True)
+def search():
+    from portfolio.models import Post
+
+    post = Post.query.get(21)
+
+    print(post.body)
+    print(type(post.body))
 
 
 if __name__ == '__main__':
