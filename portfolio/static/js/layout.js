e526ce90a45647013fcd1ce7480bfb832ee603de
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-06-17 22:45:12 -0700

    Removing enclosing document ready and shortening flash banner

--- portfolio/static/js/layout.js
+++ portfolio/static/js/layout.js
@@ -1,22 +1,19 @@
-$(document).ready(function() {
-  feather.replace();
-  $(".alert-success").delay(5000).fadeTo(2000, 500).slideUp(500, function(){
-    $(".alert-success").slideUp(500);
-  });
-  $(".alert-info").delay(5000).fadeTo(2000, 500).slideUp(500, function(){
-    $(".alert-info").slideUp(500);
-  });
-  var title = $("title").html();
-  if (title == "Home" || title == "Project Hub" ||
-      title == "Dashboard" || title == "Notebook" ||
-      title == "New Post" || title == "Edit Post") {
-    $("a.nav-link").removeClass("active");
-    $("a#"+title.replace(" ", "-")).addClass("active");
-  } else if (title.indexOf("Edit") != -1) {
-    $("a.nav-link").removeClass("active");
-    $("a#Edit-Post").addClass("active");
-  } else {
-    $("a.nav-link").removeClass("active");
-  }
-
-})
+feather.replace();
+$(".alert-success").delay(3000).fadeTo(2000, 500).slideUp(500, function(){
+  $(".alert-success").slideUp(500);
+});
+$(".alert-info").delay(3000).fadeTo(2000, 500).slideUp(500, function(){
+  $(".alert-info").slideUp(500);
+});
+var title = $("title").html();
+if (title == "Home" || title == "Project Hub" ||
+    title == "Dashboard" || title == "Notebook" ||
+    title == "New Post" || title == "Edit Post") {
+  $("a.nav-link").removeClass("active");
+  $("a#"+title.replace(" ", "-")).addClass("active");
+} else if (title.indexOf("Edit") != -1) {
+  $("a.nav-link").removeClass("active");
+  $("a#Edit-Post").addClass("active");
+} else {
+  $("a.nav-link").removeClass("active");
+};
