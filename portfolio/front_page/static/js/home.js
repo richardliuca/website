0cb4166dcd031407c191caf4e82407c726209112
Richard Liu <37211328+richardliuca@users.noreply.github.com>
2019-07-05 16:16:10 -0700

    Fixing homepage hide and fade animation

--- portfolio/front_page/static/js/home.js
+++ portfolio/front_page/static/js/home.js
@@ -1,5 +1,11 @@
-$(".jumbotron").fadeIn(1000, function() {
-  $("#self-intro").fadeIn(500, function() {
-    $(".footer").fadeIn(250);
-  });
+var loading = $('<div class="spinner-grow text-light" style="width: 3rem; height: 3rem;" role="status"></div>');
+$(loading).prependTo("body").delay(1000).fadeOut({
+  "duration": 500,
+  "start": () => {
+    $(".jumbotron").fadeIn(2000, function() {
+      $("#self-intro").fadeIn(500, function() {
+        $(".footer").fadeIn(250);
+      });
+    });
+  },
 });
