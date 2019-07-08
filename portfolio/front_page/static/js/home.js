4d1d3570f74e869c426ba44973f749f9b1444f5d
richardliuca <37211328+richardliuca@users.noreply.github.com>
2019-07-08 10:54:57 -0700

    Removing loading circle

--- portfolio/front_page/static/js/home.js
+++ portfolio/front_page/static/js/home.js
@@ -1,11 +1,8 @@
-var loading = $('<div class="spinner-grow text-light" style="width: 3rem; height: 3rem;" role="status"></div>');
-$(loading).prependTo("body").delay(1000).fadeOut({
-  "duration": 500,
-  "start": () => {
-    $(".jumbotron").fadeIn(2000, function() {
-      $("#self-intro").fadeIn(500, function() {
-        $(".footer").fadeIn(250);
-      });
+loadFrontpage();
+function loadFrontpage () {
+  $(".jumbotron").fadeIn(2000, function() {
+    $("#self-intro").fadeIn(500, function() {
+      $(".footer").fadeIn(250);
     });
-  },
-});
+  });
+};
